30eefb562e96b53f67e6132660e9d65529ccdc06
Maksym Nazarenko <maks.nazarenko@gmail.com>
2023-01-18 22:23:13 +0200

    combine SDK and Framework providers via Protocol5 mux (legacy SDK)

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"log"
 
 	"github.com/ddelnano/terraform-provider-mikrotik/mikrotik"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
 	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
+	"github.com/hashicorp/terraform-plugin-mux/tf6to5server"
 )
 
 // Generate the Terraform provider documentation using `tfplugindocs`:
@@ -20,19 +22,26 @@ func main() {
 	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	serverOpts := []tf5server.ServeOpt{}
-	if debugMode {
-		serverOpts = append(serverOpts, tf5server.WithManagedDebug())
+	ctx := context.Background()
+
+	downgradedProviderFramework, err := tf6to5server.DowngradeServer(ctx, providerserver.NewProtocol6(mikrotik.NewProviderFramework()))
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	providers := []func() tfprotov5.ProviderServer{
-		func() tfprotov5.ProviderServer { return mikrotik.NewProvider().GRPCProvider() },
+		mikrotik.NewProvider().GRPCProvider,
+		func() tfprotov5.ProviderServer { return downgradedProviderFramework },
 	}
-	ctx := context.Background()
 
 	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	serverOpts := []tf5server.ServeOpt{}
+	if debugMode {
+		serverOpts = append(serverOpts, tf5server.WithManagedDebug())
 	}
 
 	err = tf5server.Serve("registry.terraform.io/ddelnano/mikrotik", muxServer.ProviderServer, serverOpts...)
