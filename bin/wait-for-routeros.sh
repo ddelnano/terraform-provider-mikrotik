4789764c7ed674481513ff7607f2481c1e32f7f3
Maksym Nazarenko <maks.nazarenko@gmail.com>
2024-12-07 19:35:19 +0200

    refactor routeros setup for GH actions

--- bin/wait-for-routeros.sh
+++ bin/wait-for-routeros.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 routeros_host=${1:-127.0.0.1}
-routeros_port=8728
+routeros_port=${2:-8080}
 
 echo "waiting for RouterOS (${routeros_host}:${routeros_port}) to be up and running"
-
 for i in $(seq 1 60); do
-    if nc -w1 ${routeros_host} ${routeros_port}; then
+    if curl -s --connect-timeout 1 -o /dev/null ${routeros_host}:${routeros_port}; then
         exit 0;
     else
         printf "."
         sleep 1
-    fi done;
+    fi
+done;
 
 exit 1
